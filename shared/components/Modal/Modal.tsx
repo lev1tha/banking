14c7029991ff1479aa022d324028a886a22dcb45
Элдос Алмаз уулу <eldosalmazuulu@MacBook-Pro-Eldos.local>
2024-07-23 11:10:07 +0600

    added modal window, add ordered page

--- shared/components/Modal/Modal.tsx
+++ shared/components/Modal/Modal.tsx
@@ -1,14 +1,64 @@
-import React from "react";
+"use client";
+import { useEffect, useState } from "react";
 import style from "./modal.module.css";
+import { $api } from "@/shared/lib/api/api";
+import { useRouter } from "next/navigation";
 
 interface ModalPropsT {
   isSelectedDay: number | null;
 }
 
 const Modal: React.FC<ModalPropsT> = ({ isSelectedDay }) => {
+  const [dataReq, setDataReq] = useState({});
+  const route = useRouter();
+
+  useEffect(() => {
+    $api.get(`dayofweek/${isSelectedDay}`).then((req) => setDataReq(req));
+  }, [dataReq]);
+
+  const onClickRoute = () => {
+    route.push("/ordered");
+  };
   return (
     <div className={style.modal_container}>
-      Modal: <p>{isSelectedDay}</p>
+      <span>
+        Выбранный день: <h1>{isSelectedDay}</h1>
+      </span>
+      <div className="modal_information">
+        <table border={1} className={style.modal_table}>
+          <tr>
+            <td></td>
+            <td>От</td>
+            <td>До</td>
+            <td>У кого</td>
+            <td>Кем</td>
+          </tr>
+          <tr>
+            <td>Время</td>
+            <td>16:00</td>
+            <td>18:00</td>
+            <td>Женишбеков</td>
+            <td>Алмаз уулу</td>
+          </tr>
+          <tr>
+            <td>Время</td>
+            <td>16:00</td>
+            <td>18:00</td>
+            <td>Женишбеков</td>
+            <td>Маткеримов</td>
+          </tr>
+          <tr>
+            <td>Время</td>
+            <td>16:00</td>
+            <td>18:00</td>
+            <td>Женишбеков</td>
+            <td>Элдосов</td>
+          </tr>
+        </table>
+        <button className={style.button} onClick={onClickRoute}>
+          Забранировать в этот день
+        </button>
+      </div>
     </div>
   );
 };
